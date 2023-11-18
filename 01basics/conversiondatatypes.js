1c671cafd9f935f309211f9f9228b88e249a3fcc
siddeshgodkar <37167739+siddeshgodkar@users.noreply.github.com>
2023-11-18 13:07:30 +0000

    conversiondatatypes

---
+++ 01basics/conversiondatatypes.js
@@ -0,0 +1,17 @@
+let score = "Hello"
+let value = 0
+
+console.log(typeof score);
+let valueofscore = Number(score)
+console.log(typeof valueofscore)
+console.log(valueofscore)
+
+// let nullValue = String(value)
+// console.log(value);
+// console.log(typeof nullValue)
+
+
+// Null is typeof Object, null Number conversion is 0
+// NaN is typeof NaN, 
+
+//"23SS" => NaN
