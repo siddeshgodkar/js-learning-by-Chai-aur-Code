3ed441b379fac4de2e63e72f618fa8eb585b512b
siddeshgodkar <37167739+siddeshgodkar@users.noreply.github.com>
2023-11-18 12:07:58 +0000

    datatypes

---
+++ 01basics/datatypes.js
@@ -0,0 +1,9 @@
+let name = "Siddesh";
+let age = 18;
+let id;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof id);
+console.log(typeof null);
+console.log(typeof undefined);
